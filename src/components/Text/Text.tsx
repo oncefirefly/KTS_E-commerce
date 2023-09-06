23de33d11080401f303fe9ac078ef72c9d18d999
oncefirefly <oncefirefly@gmail.com>
2023-09-06 21:22:45 +0300

    feat: Text component added

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,30 @@
+import { TextProps } from 'utils/types/TextPropsTypes';
+
+import textStyles from './Text.module.scss';
+
+const Text: React.FC<TextProps> = ({ className, view, tag = 'p', weight, children, color, maxLines }: TextProps) => {
+  const CustomTag = tag;
+
+  return (
+    <CustomTag
+      className={`${className || ''}${view ? ` ${textStyles[`view-${view}`]}` : ''}${
+        weight ? ` ${textStyles[`weight-${weight}`]}` : ''
+      }${color ? ` ${textStyles[`color-${color}`]}` : ''}`}
+      style={
+        maxLines
+          ? {
+              overflow: 'hidden',
+              display: '-webkit-box',
+              WebkitLineClamp: maxLines,
+              lineClamp: maxLines,
+              WebkitBoxOrient: 'vertical',
+            }
+          : {}
+      }
+    >
+      {children}
+    </CustomTag>
+  );
+};
+
+export default Text;
