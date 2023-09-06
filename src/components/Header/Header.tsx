37781c9be8cd04b39b969e947d9fff3c152ebf49
oncefirefly <oncefirefly@gmail.com>
2023-09-06 21:26:22 +0300

    in-progress: Header component added

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+
+import Text from 'components/Text';
+import LogoIcon from 'components/icons/LogoIcon';
+
+const Header: React.FC = () => {
+  return (
+    <div>
+      <div>
+        <LogoIcon />
+        <nav>
+          <Link to="/">
+            <Text view="p-18" weight="medium">
+              Products
+            </Text>
+          </Link>
+          <Link to="/categories">
+            <Text view="p-18" weight="medium">
+              Categories
+            </Text>
+          </Link>
+          <Link to="/about_us">
+            <Text view="p-18" weight="medium">
+              About us
+            </Text>
+          </Link>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
