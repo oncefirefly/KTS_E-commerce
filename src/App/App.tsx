3edfe71bf45af40218f7a8b787948802c5839d6f
oncefirefly <oncefirefly@gmail.com>
2023-09-06 21:27:11 +0300

    upd: root coomponent updated

---
+++ src/App/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Header from 'components/Header';
+import AboutUs from './pages/AboutUs/AboutUs';
+import Categories from './pages/Categories/Categories';
+import Products from './pages/Products/Products';
+
+import './App.scss';
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/about_us" element={<AboutUs />} />{' '}
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
