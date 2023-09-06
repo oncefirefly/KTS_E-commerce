a00083f0255168932f02826eeedf5556426055bf
oncefirefly <oncefirefly@gmail.com>
2023-09-06 21:26:52 +0300

    fix: main index file styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
+
 import App from './App';
-import './index.scss';
+
+import 'styles/styles.scss';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
