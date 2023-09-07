529e9bceefed7a4572e63618e2638b06138a7797
oncefirefly <oncefirefly@gmail.com>
2023-09-07 21:48:54 +0300

    feat: useClickOutside custom hook

---
+++ src/utils/hooks/useClickOutside.ts
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+export const useClickOutside = (ref: React.RefObject<HTMLDivElement>, onClickOutside: () => void) => {
+  React.useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        onClickOutside();
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [ref, onClickOutside]);
+};
