e1d0ea20fdb89b1f39cf30adda9dbf978a127bb3
oncefirefly <oncefirefly@gmail.com>
2023-09-07 21:50:49 +0300

    fix: Products Page updated

--- src/App/pages/Products/Products.tsx
+++ src/App/pages/Products/Products.tsx
@@ -1,21 +1,23 @@
 import * as React from 'react';
 
-import Text from 'components/Text';
+import ProductsMultiDropdown from './components/ProductsMultiDropdown';
+import ProductsSearchInput from './components/ProductsSearchInput/ProductsSearchInput';
+import ProductsTitle from './components/ProductsTitle/ProductsTitle';
 
 import 'styles/styles.scss';
 import productsStyles from './Products.module.scss';
 
 const Products: React.FC = () => {
+  const handleProductsSearch = (value: string) => {
+    console.log(value);
+  };
+
   return (
     <div className={`${productsStyles.products_content} content_wrapper`}>
-      <section className={productsStyles.products_title}>
-        <Text tag="h1" view="title" color="primary" maxLines={1}>
-          Products
-        </Text>
-        <Text view="p-20" color="secondary" maxLines={2}>
-          We&nbsp;display products based on&nbsp;the latest products we&nbsp;have, if&nbsp;you want to&nbsp;see our old
-          products please enter the name of&nbsp;the item
-        </Text>
+      <ProductsTitle className={productsStyles.products_title} />
+      <section className={productsStyles.products_search_controls}>
+        <ProductsSearchInput className={productsStyles.products_search} onSearch={handleProductsSearch} />
+        <ProductsMultiDropdown />
       </section>
     </div>
   );
