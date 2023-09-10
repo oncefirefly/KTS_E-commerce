567c3aa040bf65e29cde7e53bbd152f86d3cf8c7
oncefirefly <oncefirefly@gmail.com>
2023-09-11 00:50:21 +0300

    feat: getProductById added

--- src/config/services/products/index.ts
+++ src/config/services/products/index.ts
@@ -1,9 +1,18 @@
 import { eCommerceInstance } from 'config/services/eCommerceInstance';
+import { OneProduct } from 'utils/types/ProductTypes';
 
-export const getProducts = () =>
+export const getProducts = (): Promise<OneProduct[]> =>
   eCommerceInstance
     .get('/products')
     .then((productsResponse) => productsResponse.data.slice(0, 10))
+    .catch((error) => {
+      throw new Error(error);
+    });
+
+export const getProductById = (id: string): Promise<OneProduct> =>
+  eCommerceInstance
+    .get(`/products/${id}`)
+    .then((productByIdData) => productByIdData.data)
     .catch((error) => {
       throw new Error(error);
     });
