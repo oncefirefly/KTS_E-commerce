844167a382032023e0a61c808cb8631f426fd372
oncefirefly <oncefirefly@gmail.com>
2023-09-13 22:33:09 +0300

    fix: TextTypes enums added

--- src/utils/types/TextTypes.ts
+++ src/utils/types/TextTypes.ts
@@ -1,10 +1,31 @@
+enum TextView {
+  'title',
+  'subtitle',
+  'p-20',
+  'p-18',
+  'p-16',
+  'p-14',
+}
+
+enum TextTag {
+  'h1',
+  'h2',
+  'h3',
+  'h4',
+  'h5',
+  'h6',
+  'div',
+  'p',
+  'span',
+}
+
 export type TextProps = {
   /** Дополнительный класс */
   className?: string;
   /** Стиль отображения */
-  view?: 'title' | 'button' | 'p-20' | 'p-18' | 'p-16' | 'p-14';
+  view?: keyof typeof TextView;
   /** Html-тег */
-  tag?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'div' | 'p' | 'span';
+  tag?: keyof typeof TextTag;
   /** Начертание шрифта */
   weight?: 'normal' | 'medium' | 'bold';
   /** Контент */
