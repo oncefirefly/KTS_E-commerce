6f71e02de3eb292292dbfd6ef7a1b1347d9c65fa
oncefirefly <oncefirefly@gmail.com>
2023-09-17 22:47:25 +0300

    feat: mobx Categories store

---
+++ src/store/CategoriesStore.ts
@@ -0,0 +1,29 @@
+import { action, computed, makeObservable, observable, runInAction } from 'mobx';
+import { getCategories } from 'config/services/categories';
+import { ProductCategory } from 'utils/types/ProductTypes';
+
+type PrivateCategoriesStoreFields = '_categories';
+
+export default class CategoriesStore {
+  private _categories: ProductCategory[] = [];
+
+  constructor() {
+    makeObservable<CategoriesStore, PrivateCategoriesStoreFields>(this, {
+      _categories: observable,
+      fetchCategories: action.bound,
+      categories: computed,
+    });
+  }
+
+  get categories() {
+    return this._categories;
+  }
+
+  async fetchCategories() {
+    const categoriesData = await getCategories();
+
+    runInAction(() => {
+      this._categories = categoriesData;
+    });
+  }
+}
