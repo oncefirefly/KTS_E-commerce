1c110bd33f385fbad900101b9af89ff30dbc7f06
oncefirefly <oncefirefly@gmail.com>
2023-09-17 19:01:57 +0300

    fix: ArrowLeftIcon props

--- src/components/icons/ArrowLeftIcon/ArrowLeftIcon.tsx
+++ src/components/icons/ArrowLeftIcon/ArrowLeftIcon.tsx
@@ -7,7 +7,7 @@ import { IconProps } from 'utils/types/IconTypes';
 
 export const ArrowLeftIcon: React.FC<IconProps> = (props) => {
   return (
-    <IconWrapper width="32" height="32" viewBox="0 0 32 32" fill="none">
+    <IconWrapper width="32" height="32" viewBox="0 0 32 32" fill="none" {...props}>
       <path
         d="M20.1201 26.56L11.4268 17.8667C10.4001 16.84 10.4001 15.16 11.4268 14.1333L20.1201 5.44"
         stroke={props.color ? getIconColor(props.color) : 'currentColor'}
