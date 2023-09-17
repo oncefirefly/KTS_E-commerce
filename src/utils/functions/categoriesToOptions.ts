52b89ac078f84714c2997a1215547c7a6a2327ae
oncefirefly <oncefirefly@gmail.com>
2023-09-17 22:50:56 +0300

    feat: categoriesToOptions utility function

---
+++ src/utils/functions/categoriesToOptions.ts
@@ -0,0 +1,6 @@
+import { Option } from 'utils/types/MultiDropdownTypes';
+import { ProductCategory } from 'utils/types/ProductTypes';
+
+export const categoriesToOptions = (categories: ProductCategory[]): Option[] => {
+  return categories.map((category) => ({ key: category.id, value: category.name }) as unknown as Option);
+};
