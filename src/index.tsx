75550fdb92749bc94ddab9a5b639ff3115ab4985
oncefirefly <oncefirefly@gmail.com>
2023-09-17 22:46:04 +0300

    config: mobx setup

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 
 import 'styles/styles.scss';
+import 'config/configureMobX';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
