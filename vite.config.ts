6f2719aba096eaa02650b186a27570784b8adf4b
oncefirefly <oncefirefly@gmail.com>
2023-09-18 22:40:24 +0300

    fix: config minor fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
+import path from 'path';
 import react from '@vitejs/plugin-react';
 
-import path from 'path';
+import { defineConfig } from 'vite';
 
 import tsconfig from './tsconfig.json';
 
