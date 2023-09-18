75f4b7727b681b4fe511f92198711f3acfd7d3af
oncefirefly <oncefirefly@gmail.com>
2023-09-18 22:42:14 +0300

    feat: Multidropdown filter on Products Page

--- src/App/pages/Products/Products.tsx
+++ src/App/pages/Products/Products.tsx
@@ -4,53 +4,84 @@ import { useSearchParams } from 'react-router-dom';
 
 import ProductsStore from 'store/ProductsStore';
 
+import { Option } from 'utils/types/MultiDropdownTypes';
+
 import { ProductsList, ProductsMultiDropdown, ProductsSearchInput, ProductsTitle } from './components';
 
 import productsStyles from './Products.module.scss';
 
+let renderCount = 0;
+
 export const Products: React.FC = observer(() => {
+  console.log('render', ++renderCount);
+
   const pageFromLocalStorage = localStorage.getItem('eCommerceProductsPage');
+  const searchValueFromLocalStorage = localStorage.getItem('eCommerceProductsSearch');
+  const categoriesFromLocalStorage = JSON.parse(localStorage.getItem('eCommerceProductsCategories') as string);
 
   const [searchParams, setSearchParams] = useSearchParams();
+  const [searchValue, setSearchValue] = React.useState<string>(searchValueFromLocalStorage || '');
+  const [selectedCategories, setSelectedCategories] = React.useState<Option[]>(categoriesFromLocalStorage || []);
   const [currentPage, setCurrentPage] = React.useState<number>(pageFromLocalStorage ? +pageFromLocalStorage : 1);
 
   const productsStore = React.useMemo(() => {
     return new ProductsStore();
   }, []);
 
-  // TODO: search filter
-  const handleProductsSearch = (value: string) => {
-    productsStore.filterProductsOnSearch(value);
-  };
+  const handleProductsSearch = React.useCallback(
+    (value: string) => {
+      productsStore.filterProductsOnSearch(value);
+
+      setSearchValue(value);
+      setCurrentPage(1);
+    },
+    [productsStore],
+  );
+
+  const handleCategoryFilter = React.useCallback(
+    (value: Option[]) => {
+      setSelectedCategories(value);
+      const selectedOptions = value.map((option) => option.key).join('|');
+
+      productsStore.fetchProducts(selectedOptions);
+    },
+    [productsStore],
+  );
 
   React.useEffect(() => {
-    const fetchProducts = async () => {
+    const setupProducts = async () => {
+      console.log('fetching...');
+
       await productsStore.fetchProducts();
     };
 
-    fetchProducts();
+    setupProducts();
   }, [productsStore]);
+
+  React.useEffect(() => {
+    const selectedCategoriesKeys = selectedCategories.map((category) => category.key).join('|');
+
+    localStorage.setItem('eCommerceProductsPage', currentPage.toString());
+    localStorage.setItem('eCommerceProductsSearch', searchValue);
+    localStorage.setItem('eCommerceProductsCategories', JSON.stringify(selectedCategories));
+
+    setSearchParams({ page: currentPage.toString(), search: searchValue, categories: selectedCategoriesKeys });
+  }, [currentPage, searchValue, selectedCategories, setSearchParams]);
 
   React.useEffect(() => {
     const searchParamsData = Object.fromEntries(searchParams.entries());
 
-    if (searchParamsData.page) {
-      setCurrentPage(+searchParamsData.page);
-    }
+    setCurrentPage(+searchParamsData.page);
+
+    setSearchValue(searchParamsData.search);
   }, [searchParams]);
-
-  React.useEffect(() => {
-    localStorage.setItem('eCommerceProductsPage', currentPage.toString());
-
-    setSearchParams({ page: currentPage.toString() });
-  }, [currentPage, setSearchParams]);
 
   return (
     <div className={`${productsStyles.products_content} content_wrapper`}>
       <ProductsTitle className={productsStyles.products_title} />
       <section className={productsStyles.products_search_controls}>
         <ProductsSearchInput className={productsStyles.products_search} onSearch={handleProductsSearch} />
-        <ProductsMultiDropdown />
+        <ProductsMultiDropdown selectedOptions={selectedCategories} onChange={handleCategoryFilter} />
       </section>
       <ProductsList
         className={productsStyles.products_list}
