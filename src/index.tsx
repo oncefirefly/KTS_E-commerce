878d7a54e8778554e9e9e4c1a9c532b28017274b
oncefirefly <oncefirefly@gmail.com>
2023-09-27 00:22:51 +0300

    fix: added 'regenerator-runtime' import

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
-
+import 'regenerator-runtime';
 import '@styles/styles.scss';
 import '@config/configureMobX';
 
