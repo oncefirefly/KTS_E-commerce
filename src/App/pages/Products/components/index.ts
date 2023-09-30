63da83d6e8c557e15367d8b14c3ac6d83c46382c
oncefirefly <oncefirefly@gmail.com>
2023-10-01 00:48:07 +0300

    fix: `ProductsTitle` export removed

--- src/App/pages/Products/components/index.ts
+++ src/App/pages/Products/components/index.ts
@@ -1,4 +1,3 @@
 export { ProductsCount } from './ProductsCount/ProductsCount';
 export { ProductsList } from './ProductsList/ProductsList';
 export { ProductsSearchInput } from './ProductsSearchInput/ProductsSearchInput';
-export { ProductsTitle } from './ProductsTitle/ProductsTitle';
