b553699986abfc2f18438f309712f887f5c597ba
oncefirefly <oncefirefly@gmail.com>
2023-10-01 00:45:27 +0300

    fix: `Icons` components

--- src/components/icons/ArrowDownIcon/ArrowDownIcon.tsx
+++ src/components/icons/ArrowDownIcon/ArrowDownIcon.tsx
@@ -6,14 +6,14 @@ import { getIconColor } from '@utils/functions/getIconColor';
 
 import { IconProps } from '@utils/types/IconTypes';
 
-export const ArrowDownIcon: React.FC<IconProps> = (props) => {
+export const ArrowDownIcon: React.FC<IconProps> = ({ className, color }) => {
   return (
-    <IconWrapper width="25" height="24" viewBox="0 0 25 24" fill="none">
+    <IconWrapper className={className || ''} width="25" height="24" viewBox="0 0 25 24" fill="none">
       <path
         fillRule="evenodd"
         clipRule="evenodd"
         d="M2.33563 8.74741L3.66436 7.25259L12 14.662L20.3356 7.25259L21.6644 8.74741L12 17.338L2.33563 8.74741Z"
-        fill={props.color ? getIconColor(props.color) : 'currentColor'}
+        fill={color ? getIconColor(color) : 'currentColor'}
       />
     </IconWrapper>
   );
--- src/components/icons/IconWrapper/IconWrapper.tsx
+++ src/components/icons/IconWrapper/IconWrapper.tsx
@@ -12,7 +12,7 @@ export const IconWrapper: React.FC<React.PropsWithChildren<IconProps>> = ({
   ...props
 }) => {
   return (
-    <svg className={className} width={width || 24} height={height || 24} viewBox={viewBox} fill={fill} {...props}>
+    <svg className={className || ''} width={width || 24} height={height || 24} viewBox={viewBox} fill={fill} {...props}>
       {children}
     </svg>
   );
