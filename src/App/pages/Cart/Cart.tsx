bbc00ecc5cf7617485a8ab2d5b256b7f27631654
oncefirefly <oncefirefly@gmail.com>
2023-10-01 22:42:18 +0300

    feat: Cart Page

---
+++ src/App/pages/Cart/Cart.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export const Cart: React.FC = () => {
+  return <div>cart</div>;
+};
--- src/App/pages/index.ts
+++ src/App/pages/index.ts
@@ -1,4 +1,5 @@
 export { AboutUs } from './AboutUs/AboutUs';
+export { Cart } from './Cart/Cart';
 export { Categories } from './Categories/Categories';
 export { Login } from './Login/Login';
 export { Product } from './Product/Product';
