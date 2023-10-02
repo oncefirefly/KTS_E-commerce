1f66f2753ad8e739e677a0d85a7278545bddd0a0
oncefirefly <oncefirefly@gmail.com>
2023-10-02 21:10:49 +0300

    fixL removed unused types

--- src/utils/types/HeaderTypes.ts
+++
@@ -1,3 +0,0 @@
-export type HeaderProps = {
-  userName?: string;
-};
--- src/utils/types/LoginTypes.ts
+++
@@ -1,9 +0,0 @@
-export type LoginData = {
-  isLoggedIn?: boolean;
-  email: string;
-  uid: string;
-};
-
-export type LoginPageProps = {
-  onLogin: (data: LoginData) => void;
-};
