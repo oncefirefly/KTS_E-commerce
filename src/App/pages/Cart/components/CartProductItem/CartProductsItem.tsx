723f021c8bff8c46e612f6a46a4776a3b52ce9ee
oncefirefly <oncefirefly@gmail.com>
2023-10-04 22:08:14 +0300

    fix: product page on cart product click

--- src/App/pages/Cart/components/CartProductItem/CartProductsItem.tsx
+++ src/App/pages/Cart/components/CartProductItem/CartProductsItem.tsx
@@ -1,5 +1,6 @@
 import { observer } from 'mobx-react-lite';
 import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { Text } from '@components/index';
 
@@ -10,8 +11,10 @@ import { CartItemProps } from '@utils/types/CartTypes';
 import cartItemStyles from './CartProductsItem.module.scss';
 
 export const CartProductsItem: React.FC<CartItemProps> = observer(({ product }) => {
+  const navigate = useNavigate();
+
   return (
-    <div className={cartItemStyles.cart_item}>
+    <div className={cartItemStyles.cart_item} onClick={() => navigate(`/product/${product.id}`)}>
       <img className={cartItemStyles.cart_item_thumbnail} src={product.images[0]} />
       <div className={cartItemStyles.cart_item_title}>
         <Text view="p-20" weight="medium">
@@ -22,11 +25,24 @@ export const CartProductsItem: React.FC<CartItemProps> = observer(({ product }) 
         </Text>
       </div>
       <div className={cartItemStyles.cart_item_quantity}>
-        <button onClick={() => cartStore.substractQuantity(product)} disabled={product.quantity === 0}>
+        <button
+          onClick={(e) => {
+            e.stopPropagation();
+            cartStore.substractQuantity(product);
+          }}
+          disabled={product.quantity === 0}
+        >
           -
         </button>
         <Text>{product.quantity}</Text>
-        <button onClick={() => cartStore.addQuantity(product)}>+</button>
+        <button
+          onClick={(e) => {
+            e.stopPropagation();
+            cartStore.addQuantity(product);
+          }}
+        >
+          +
+        </button>
       </div>
       <Text className={cartItemStyles.cart_item_price} view="p-16" weight="medium" color="accent">
         {product.price * product.quantity} $
